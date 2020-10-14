9f25594da1634a854c8543eb785a75a394a21884
kulvait <kulvait@gmail.com>
2020-10-14 11:39:26 +0200

    dentk-noise Poisson support

--- src/dentk-noise.cpp
+++ src/dentk-noise.cpp
@@ -3,6 +3,7 @@
 
 // External libraries
 #include <algorithm>
+#include <cmath>
 #include <cstdlib>
 #include <ctype.h>
 #include <iostream>
@@ -13,6 +14,7 @@
 // External libraries
 #include "CLI/CLI.hpp" //Command line parser
 #include "ctpl_stl.h" //Threadpool
+#include "mkl.h"
 
 // Internal libraries
 #include "AsyncFrame2DWritterI.hpp"
@@ -43,8 +45,11 @@ public:
         , ArgumentsThreading(argc, argv, prgName){};
     std::string inputFile;
     std::string outputFile;
+    bool gauss = false;
     double standardDeviation = 1.0;
     double mean = 0.0;
+    bool poisson = false;
+    double K0 = 7500.0;
     uint32_t dimx;
     uint32_t dimy;
     uint32_t dimz;
@@ -60,12 +65,33 @@ void Args::defineArguments()
         ->required();
     cliApp->add_flag("--force", force, "Overwrite outputFile if it exists.");
     CLI::Option_group* op_clg
-        = cliApp->add_option_group("Parameters", "Add parameters of the standard distribution.");
+        = cliApp->add_option_group("Noise type", "Choose the type of the noise.");
+    CLI::Option* gaussDistribution
+        = op_clg->add_flag("--gauss", gauss, "Gauss distribution is added to the data");
+    CLI::Option* poissonDistribution
+        = op_clg->add_flag("--poisson", poisson,
+                           "Poisson noise with lambda=K. Data are transformed to "
+                           "compute K=K0exp(-x), poisson value L is generated, "
+                           "then x is substituted by ln(K0/L).");
+    op_clg->require_option(1);
+    op_clg = cliApp->add_option_group("Gauss distribution parameters",
+                                      "Parameters of the standard distribution.");
     std::string optSpec;
     optSpec = io::xprintf("Standard distribution mean, [defaults to %f].", mean);
-    op_clg->add_option("--mean", mean, optSpec);
+    CLI::Option* gaussMean = op_clg->add_option("--mean", mean, optSpec);
     optSpec = io::xprintf("Standard distribution sigma, [defaults to %f].", standardDeviation);
-    op_clg->add_option("--standard-deviation", standardDeviation, optSpec);
+    CLI::Option* gaussSigma
+        = op_clg->add_option("--standard-deviation", standardDeviation, optSpec);
+    op_clg = cliApp->add_option_group("Poisson distribution parameters",
+                                      "Parameters of the poisson distribution.");
+    optSpec = io::xprintf("Poisson lambda that equals K0, number of photons per blank scan "
+                          "detector [defaults to %f].",
+                          K0);
+    CLI::Option* poissonLambda = op_clg->add_option("--k0", mean, optSpec);
+    poissonLambda->needs(poissonDistribution);
+    gaussMean->needs(gaussDistribution);
+    gaussSigma->needs(gaussDistribution);
+
     addFramespecArgs();
     addThreadingArgs();
 }
@@ -93,7 +119,7 @@ int Args::postParse()
 }
 
 template <typename T>
-void addNoiseFrame(int id,
+void addGaussFrame(int id,
                    int fromId,
                    std::shared_ptr<io::Frame2DReaderI<T>> inputReader,
                    int toId,
@@ -117,16 +143,70 @@ void addNoiseFrame(int id,
         for(uint32_t x = 0; x != dimx; x++)
         {
             noise[y * dimx + x] += curframe->get(x, y);
-            if( noise[y * dimx + x] < 0.0)
-			{
-				noise[y * dimx + x] = 0.0;
-			}
+            if(noise[y * dimx + x] < 0.0)
+            {
+                noise[y * dimx + x] = 0.0;
+            }
         }
     }
     std::unique_ptr<io::Frame2DI<T>> f
         = std::make_unique<io::FrameMemoryViewer2D<T>>(noise, dimx, dimy);
     imagesWritter->writeFrame(*f, toId);
     delete[] noise;
+}
+
+template <typename T>
+void addPoissonFrame(int id,
+                     int fromId,
+                     std::shared_ptr<io::Frame2DReaderI<T>> inputReader,
+                     int toId,
+                     std::shared_ptr<io::AsyncFrame2DWritterI<T>> imagesWritter,
+                     double k0lambda)
+{
+    uint32_t dimx = imagesWritter->dimx();
+    uint32_t dimy = imagesWritter->dimy();
+    uint32_t frameSize = dimx * dimy;
+    MKL_UINT seed = fromId;
+    MKL_INT status;
+    VSLStreamStatePtr stream;
+    vslNewStream(&stream, VSL_BRNG_MCG31, seed);
+    T* frame = new T[frameSize];
+    double K;
+    int L;
+    std::shared_ptr<io::Frame2DI<T>> curframe = inputReader->readFrame(fromId);
+    for(uint32_t y = 0; y != dimy; y++)
+    {
+        for(uint32_t x = 0; x != dimx; x++)
+        {
+            K = k0lambda * exp(-curframe->get(x, y));
+            status = viRngPoissonV(VSL_RNG_METHOD_POISSONV_POISNORM, stream, 1, &L, &K);
+            if(status == VSL_STATUS_OK)
+            {
+                if(L > k0lambda)
+                {
+                    frame[y * dimx + x] = 0;
+                } else if(L == 0)
+                {
+                    frame[y * dimx + x] = log(k0lambda/0.5);
+                }else
+                {
+                    frame[y * dimx + x] = log(k0lambda / L);
+                }
+            } else
+            {
+                std::string ERR = io::xprintf(
+                    "The status for frame fromID %d toID %d x=%d y=%d is not OK and is %d", fromId,
+                    toId, x, y, status);
+                LOGE << ERR;
+                throw std::runtime_error(ERR);
+            }
+        }
+    }
+    vslDeleteStream(&stream);
+    std::unique_ptr<io::Frame2DI<T>> f
+        = std::make_unique<io::FrameMemoryViewer2D<T>>(frame, dimx, dimy);
+    imagesWritter->writeFrame(*f, toId);
+    delete[] frame;
 }
 
 template <typename T>
@@ -148,11 +228,27 @@ void elementWiseNoise(Args a)
     {
         if(threadpool != nullptr)
         {
-            threadpool->push(addNoiseFrame<T>, a.frames[i], inputReader, i, imagesWritter, a.mean,
-                             a.standardDeviation);
+            if(a.gauss)
+            {
+                threadpool->push(addGaussFrame<T>, a.frames[i], inputReader, i, imagesWritter,
+                                 a.mean, a.standardDeviation);
+            }
+            if(a.poisson)
+            {
+                threadpool->push(addPoissonFrame<T>, a.frames[i], inputReader, i, imagesWritter,
+                                 a.K0);
+            }
         } else
         {
-            addNoiseFrame<T>(0, a.frames[i], inputReader, i, imagesWritter, a.mean, a.standardDeviation);
+            if(a.gauss)
+            {
+                addGaussFrame<T>(0, a.frames[i], inputReader, i, imagesWritter, a.mean,
+                                 a.standardDeviation);
+            }
+            if(a.poisson)
+            {
+                addPoissonFrame<T>(0, a.frames[i], inputReader, i, imagesWritter, a.K0);
+            }
         }
     }
     if(threadpool != nullptr)
@@ -166,7 +262,8 @@ int main(int argc, char* argv[])
 {
     Program PRG(argc, argv);
     // Argument parsing
-    Args ARG(argc, argv, "Add a normally distributed noise to a DEN file.");
+    Args ARG(argc, argv,
+             "Add a noise to a DEN file, normal or Poisson distributed on transformed data.");
     // Argument parsing
     int parseResult = ARG.parse();
     if(parseResult > 0)
@@ -194,7 +291,7 @@ int main(int argc, char* argv[])
     }
     case io::DenSupportedType::uint16_t_:
     {
-    	//elementWiseNoise<uint16_t>(ARG);
+        // elementWiseNoise<uint16_t>(ARG);
         std::string errMsg
             = io::xprintf("Unsupported data type %s.", io::DenSupportedTypeToString(dataType));
         LOGE << errMsg;
