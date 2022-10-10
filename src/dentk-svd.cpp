09740c86bbd82a1d4729d37e403b68d7575ab416
Vojtěch Kulvait <kulvait@gmail.com>
2022-10-10 10:18:17 +0200

    SVD computations on big data
    *Changed type of procedure to process larger datasets
    *Deleting memory before program ends

--- src/dentk-svd.cpp
+++ src/dentk-svd.cpp
@@ -74,9 +74,9 @@ int main(int argc, char* argv[])
         }
     }
     io::DenFileInfo di(a.input_file);
-    uint32_t dimx = di.dimx();
-    uint32_t dimy = di.dimy();
-    uint32_t dimz = di.dimz();
+    uint64_t dimx = di.dimx();
+    uint64_t dimy = di.dimy();
+    uint64_t dimz = di.dimz();
     uint64_t offset = di.getOffset();
     // int elementSize = di.elementByteSize();
     io::DenSupportedType t = di.getDataType();
@@ -97,41 +97,120 @@ int main(int argc, char* argv[])
         {
             ERR = io::xprintf("Uint32 is exceeded by the byte size of matrix that is %lu.",
                               totalSize * sizeof(float));
-            KCTERR(ERR);
+            LOGW << ERR;
         }
-        float* A = new float[totalSize]; // First going to read my matrix
-        io::readBytesFrom(a.input_file, offset, (uint8_t*)A,
-                          totalSize * sizeof(float)); // Quite hard to process
-        float *U, *S, *V;
-        U = nullptr;
-        V = nullptr;
-        S = nullptr;
-        float* superb;
-        char jobu = 'N', jobvt = 'N';
-        int ldu = 0, ldvt = 0;
         bool storeU = !a.U_file.empty();
         bool storeV = !a.V_file.empty();
-        uint32_t numvectors = std::min(dimx * dimy, dimz);
-        S = new float[numvectors];
-        superb = new float[numvectors - 1];
-        ldu = numvectors;
-        ldvt = dimx * dimy;
-        if(storeU)
+        // Parameters of gesvd
+        float *A = nullptr, *U = nullptr, *S = nullptr, *V = nullptr, *superb = nullptr;
+        lapack_int* superbx = nullptr;
+        char jobu, jobvt;
+        lapack_int m, n, lda, ldu, ldvt;
+        // Initialization
+        LOGI << io::xprintf("Allocating vector A of the size %lu", totalSize);
+        A = new float[totalSize]; // First going to read my matrix
+        io::readBytesFrom(a.input_file, offset, (uint8_t*)A,
+                          totalSize * sizeof(float)); // Quite hard to process
+        lapack_int inf;
+        bool LAPACKE_sgesvdx_algorithm = true;
+        if(LAPACKE_sgesvdx_algorithm == false)
         {
-            U = new float[dimz * numvectors];
-            jobu = 'S';
-        }
-        if(storeV)
+            // This implementation has limitations when m is large so it is here for reference
+            m = dimz;
+            n = dimx * dimy;
+            lda = n;
+            ldu = std::min(m, n); // Number of vectors stored
+            ldvt = n;
+            if(storeU)
+            {
+                jobu = 'S';
+                uint64_t u_size = (uint64_t)ldu * (uint64_t)m;
+                LOGI << io::xprintf("Allocating vector U of the size %lu, where m=%d ldu=%d",
+                                    u_size, m, ldu);
+                U = new float[u_size];
+            } else
+            {
+                jobu = 'N';
+                U = nullptr;
+            }
+            if(storeV)
+            {
+                jobvt = 'S';
+                // The following is required by specs despite the fact that it will allocate huge
+                // array
+                // https://www.intel.com/content/www/us/en/develop/documentation/onemkl-developer-reference-c/top/lapack-routines/lapack-least-squares-and-eigenvalue-problem/lapack-least-squares-eigenvalue-problem-driver/singular-value-decomposition-lapack-driver/gesvd.html
+                uint64_t v_size = (uint64_t)ldvt * (uint64_t)n;
+                LOGI << io::xprintf("Allocating vector V of the size %lu, where ldu=%d n=%d",
+                                    v_size, ldu, n);
+                V = new float[v_size];
+            } else
+            {
+                jobvt = 'N';
+                V = nullptr;
+            }
+            S = new float[ldu];
+            superb = new float[ldu - 1];
+            LOGD << io::xprintf("Calling LAPACK_ROW_MAJOR LAPACKE_sgesvd with dimx*dimy=%d dimz=%d",
+                                dimx * dimy, dimz);
+            LOGI << io::xprintf("jobu=%c, jobvt=%c, m=%d, n=%d, lda=%d, ldu=%d, ldvt=%d", jobu,
+                                jobvt, dimz, dimx * dimy, dimx * dimy, ldu, ldvt);
+            // lapack_int inf = LAPACKE_sgesvd(LAPACK_ROW_MAJOR, jobu, jobvt, dimz, dimx * dimy, A,
+            //                                dimx * dimy, S, U, ldu, V, ldvt, superb);
+            inf = LAPACKE_sgesvd(LAPACK_ROW_MAJOR, jobu, jobvt, m, n, A, lda, S, U, ldu, V, ldvt,
+                                 superb);
+        } else
         {
-            V = new float[dimx * dimy * numvectors];
-            jobvt = 'S';
+            // Documentation
+            // https://www.intel.com/content/www/us/en/develop/documentation/onemkl-developer-reference-c/top/lapack-routines/lapack-least-squares-and-eigenvalue-problem/lapack-least-squares-eigenvalue-problem-driver/singular-value-decomposition-lapack-driver/gesvdx.html
+            char range = 'A';
+            float vl = 0.0f, vu = 0.0f;
+            lapack_int il = 0, iu = 0;
+            lapack_int ns;
+            m = dimz;
+            n = dimx * dimy;
+            lda = n;
+            ldu = std::min(m, n);
+            ldvt = n;
+            if(a.max_s_vals < ldu && a.max_s_vals > 0)
+            {
+                range = 'I';
+                il = 1;
+                iu = il + a.max_s_vals - 1;
+                ldu = std::min((int)ldu, (int)a.max_s_vals);
+            }
+            if(storeU)
+            {
+                jobu = 'V';
+                uint64_t u_size = (uint64_t)ldu * (uint64_t)m;
+                LOGI << io::xprintf("Allocating vector U of the size %lu, where m=%d ldu=%d",
+                                    u_size, m, ldu);
+                U = new float[u_size];
+            } else
+            {
+                jobu = 'N';
+                U = nullptr;
+            }
+            if(storeV)
+            {
+                jobvt = 'V';
+                uint64_t v_size = (uint64_t)ldu * (uint64_t)n;
+                LOGI << io::xprintf("Allocating vector V of the size %lu, where ldu=%d n=%d",
+                                    v_size, ldu, n);
+                V = new float[v_size];
+            } else
+            {
+                jobvt = 'N';
+                V = nullptr;
+            }
+            S = new float[ldu];
+            superbx = new lapack_int[12 * ldu];
+            inf = LAPACKE_sgesvdx(LAPACK_ROW_MAJOR, jobu, jobvt, range, m, n, A, lda, vl, vu, il,
+                                  iu, &ns, S, U, ldu, V, ldvt, superbx);
         }
-        LOGD << io::xprintf("Calling LAPACKE_sgesvd with dimx*dimy=%d dimz=%d", dimx * dimy, dimz);
-        int inf = LAPACKE_sgesvd(LAPACK_ROW_MAJOR, jobu, jobvt, dimz, dimx * dimy, A, dimx * dimy,
-                                 S, U, ldu, V, ldvt, superb);
-        LOGI << "After LAPACKE_sgesvd.";
         if(inf == 0)
         {
+            LOGI << "SVD computed successfully.";
+            uint32_t numvectors = ldu;
             uint32_t numreturned = std::min(numvectors, a.max_s_vals);
             if(a.explain_procent < 100.0)
             {
@@ -164,6 +243,7 @@ int main(int argc, char* argv[])
             }
             if(storeU)
             {
+                LOGI << io::xprintf("Storing U vector to the file %s", a.U_file.c_str());
                 std::shared_ptr<io::AsyncFrame2DWritterI<float>> Uw
                     = std::make_shared<io::DenAsyncFrame2DWritter<float>>(a.U_file, dimz, 1,
                                                                           numreturned);
@@ -179,12 +259,13 @@ int main(int argc, char* argv[])
             }
             if(storeV)
             {
+                LOGI << io::xprintf("Storing V vector to the file %s", a.V_file.c_str());
                 std::shared_ptr<io::AsyncFrame2DWritterI<float>> Vw
-                    = std::make_shared<io::DenAsyncFrame2DWritter<float>>(a.V_file, dimx * dimy, 1,
+                    = std::make_shared<io::DenAsyncFrame2DWritter<float>>(a.V_file, dimx, dimy,
                                                                           numreturned);
-                for(uint32_t i = 0; i != numreturned; i++)
+                for(uint64_t i = 0; i != numreturned; i++)
                 {
-                    io::BufferedFrame2D<float> s(&V[dimx * dimy * i], 1, dimx * dimy);
+                    io::BufferedFrame2D<float> s(&V[dimx * dimy * i], dimx, dimy);
                     Vw->writeFrame(s, i);
                 }
             }
@@ -192,8 +273,31 @@ int main(int argc, char* argv[])
         } else
         {
             std::string msg = io::xprintf("There was a convergence problem catched inf=%d.", inf);
-            LOGE << msg;
-            throw std::runtime_error(msg);
+            KCTERR(msg);
+        }
+        if(A != nullptr)
+        {
+            delete[] A;
+        }
+        if(U != nullptr)
+        {
+            delete[] U;
+        }
+        if(S != nullptr)
+        {
+            delete[] S;
+        }
+        if(V != nullptr)
+        {
+            delete[] V;
+        }
+        if(superb != nullptr)
+        {
+            delete[] superb;
+        }
+        if(superbx != nullptr)
+        {
+            delete[] superbx;
         }
         break;
     }
